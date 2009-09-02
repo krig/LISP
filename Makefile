cbd55f95f32170f2469340de4865dac283365e2b
Kristoffer Grönlund <kristoffer.gronlund@purplescout.se>
2009-09-02 08:32:41 +0200

    hum hum

--- Makefile
+++ Makefile
@@ -1,16 +1,18 @@
-CC=gcc
-CFLAGS=-Wall -Werror -g -std=c99
-DEPS = stream.h
-OBJ = main.o stream.o
-
 glib_cflags = `pkg-config --cflags glib-2.0`
 glib_libs = `pkg-config --libs glib-2.0`
 
+CC=gcc
+CFLAGS=-Wall -Werror -g -std=c99 $(glib_cflags)
+DEPS = stream.h
+OBJ = main.o stream.o
+LIBS = -lgc $(glib_libs)
+
+
 %.o: %.c $(DEPS)
-	$(CC) $(glib_cflags) -c -o $@ $< $(CFLAGS)
+	$(CC) -c -o $@ $< $(CFLAGS)
 
 main: $(OBJ)
-	$(CC) -o $@ $^ $(glib_cflags) $(CFLAGS) $(glib_libs)
+	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
 
 .PHONY: clean
 
--- main.c
+++ main.c
@@ -2,7 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <glib.h>
+#include <gc/gc.h>
 #include "stream.h"
+
+struct Word;
+
+typedef union {
+        unsigned int umask;
+        struct Word* word;
+} address_t;
+
+typedef union {
+        struct Word* word;
+        int num;
+        const char* str;
+} decrement_t;
+
+struct Word {
+        address_t address;
+        decrement_t decrement;
+};
+
+#define WordAtomP(w) ((w)->address.umask &= 1)
+#define WordAtomType(w) ((w)->address.umask & 0x00000006)
+
+#define WORDATOM_WORD 0
+#define WORDATOM_NUM 2
+#define WORDATOM_STR 6
 
 /*
   COND:
@@ -10,23 +36,62 @@
   output: first value whose expression evaluates to true
  */
 
-struct cons_cell {
-        struct cons_cell* car;
-        struct cons_cell* cdr;
+struct slot { /* represents a value */
+        unsigned int type;
+        union {
+                int number;
+                const char* str;
+                void* voidp;
+        } data;
 };
+
+#define SLOT_SYMBOL 0
+#define SLOT_NUMBER 1
+#define SLOT_STRING 2
+#define SLOT_DATA 3
+
+typedef union {
+        struct slot* atom;
+        struct pair* pair;
+} pair_entry_t;
+
+struct pair {
+        pair_entry_t car;
+        pair_entry_t cdr;
+        unsigned int meta;
+};
+
+#define car_atom_p(p) (((p)->meta)&1)
+#define car_pair_p(p) ((((p)->meta)&1) == 0)
+#define cdr_atom_p(p) (((p)->meta)&2)
+#define cdr_pair_p(p) ((((p)->meta)&2) == 0)
+#define car_atom(p) ((p)->car.atom)
+#define car_pair(p) ((p)->car.pair)
+#define cdr_atom(p) ((p)->cdr.atom)
+#define cdr_pair(p) ((p)->cdr.pair)
+#define car_nil_p(p) ((p)->car.pair == NULL)
+#define set_car_atom(p, a) do { struct pair* _setptr = (p); _setptr->meta |= 1; _setptr->car.atom = (a); } while(0)
+#define set_cdr_atom(p, a) do { struct pair* _setptr = (p); _setptr->meta |= 2; _setptr->cdr.atom = (a); } while(0)
+#define set_car_pair(p, r) do { struct pair* _setptr = (p); _setptr->meta &= ~1; _setptr->car.pair = (r); } while(0)
+#define set_cdr_pair(p, r) do { struct pair* _setptr = (p); _setptr->meta &= ~2; _setptr->cdr.pair = (r); } while(0)
+#define set_nil_pair(p) do { struct pair* _setptr = (p); _setptr->meta = 0; _setptr->car.pair = _setptr->cdr.pair = NULL; } while(0)
 
 struct interpreter {
         struct instream stream;
+        struct slot* T;
 };
+
+const char* intern(const char* str) {
+        return g_intern_string(str);
+}
 
 gboolean init_interpreter(struct interpreter* I, const char* filename) {
         memset(I, 0, sizeof(struct interpreter));
         init_file_stream(&I->stream, filename);
+        I->T = GC_MALLOC(sizeof(struct slot));
+        I->T->type = SLOT_SYMBOL;
+        I->T->data.str = intern("true");
         return TRUE;
-}
-
-const char* intern(const char* str) {
-        return g_intern_string(str);
 }
 
 int get_char(struct interpreter* I) {
@@ -42,5 +107,32 @@ int main(int argc, char* argv[]) {
         struct interpreter* I = &i;
         init_interpreter(I, argv[1]);
         printf("%c\n", get_char(I));
+
+        struct slot* s1 = GC_MALLOC(sizeof(struct slot));
+        struct slot* s2 = GC_MALLOC(sizeof(struct slot));
+
+        struct pair* p1 = GC_MALLOC(sizeof(struct pair));
+        struct pair* p2 = GC_MALLOC(sizeof(struct pair));
+
+        set_nil_pair(p1);
+        set_nil_pair(p2);
+        set_car_pair(p1, p2);
+        set_cdr_atom(p1, s1);
+        set_car_atom(p2, s2);
+        set_cdr_pair(p2, NULL);
+
+        printf("(car %d/%d) (cdr %d/%d) (%u)\n",
+               car_atom_p(p1),
+               car_pair_p(p1),
+               cdr_atom_p(p1),
+               cdr_pair_p(p1),
+               p1->meta);
+        printf("(car %d/%d) (cdr %d/%d) (%u)\n",
+               car_atom_p(p2),
+               car_pair_p(p2),
+               cdr_atom_p(p2),
+               cdr_pair_p(p2),
+               p2->meta);
+        
         return 0;
 }
