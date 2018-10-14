3b4dcd61fb5ef2ef96db18b36331f56337b87ce4
Kristoffer Grönlund <krig@koru.se>
2018-10-14 10:31:13 +0200

    Add old.scm which sort of implements test.lisp from old-version

---
+++ tests/old.scm
@@ -0,0 +1,21 @@
+(define atom? (lambda (x) (cond ((null? x) #f) ((pair? x) #f) (#t #t))))
+(define cadr (lambda (x) (car (cdr x))))
+(define displayln (lambda (x) (display x) (newline)))
+
+(displayln (quote hello-world))
+(displayln (car (quote (hello-world goodbye-world))))
+(displayln (cadr (quote (hello-world goodbye-world))))
+(displayln ((lambda (x) (x (quote (1 2)))) car))
+(displayln ((lambda (x) (x (quote (1 2)))) cadr))
+
+(define ff (lambda (x)
+             (cond
+              ((pair? x) (ff (car x)))
+              (#t x))))
+
+(define mapcar (lambda (f l)
+                 (cond
+                  ((null? l) #f)
+                  (#t (cons (f (car l)) (mapcar f (cdr l)))))))
+
+(displayln (mapcar ff (list 1 (list 2 3))))
